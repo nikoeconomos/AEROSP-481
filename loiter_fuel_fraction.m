0d9d1f22329eee2921bd9eec1ac79dbc48bb1113
Juan Esteban Vega <juanvega@umich.edu>
2024-09-10 20:41:22 -0400

    Commented loiter_fuel_fraction

--- loiter_fuel_fraction.m
+++ loiter_fuel_fraction.m
@@ -1,20 +1,25 @@
 function [segment_fuel_fraction] = loiter_fuel_fraction(segment_endurance,specific_fuel_capacity,lift_to_drag)
-% Description: This function generates a the TOGW of our aircraft by calling
-% another function, togw_regression_loop. It runs the regression loop 3 times for all 
-% three missions and returns three separate TOGWs.
+% Description: This function calculates the fuel fraction for a given
+% loiter time when steady flight can be assumed. It uses equation 2.8 from the 
+% metabook meant to calculate loiter fuel fraction
 % 
 % INPUTS:
 % --------------------------------------------
-%    weight_params - Struct defined in generate_weight_params function.
-%    Contains necessary parameters for the calculation.
+%    segment_endurance - Double defined in fuel_weight function. 
+%    Loitering time range [s] 
+% 
+%    specific_fuel_comsumption - Double defined in section 1 of main.m 
+%    Assumed specific fuel consumption for the PW F100-229 engine [kg/(kg*hr)]
+% 
+%    lift_to_drag -  Double defined in max_lift_to_drag function. 
+%    Aircraft lift to drag ratio [unitless]
 % 
 % OUTPUTS:
 % --------------------------------------------
-%    togw_DCA - TOGW for DCA [lbs]
-%    togw_PDI - TOGW for PDI [lbs]
-%    togw_ESCORT - TOGW for escort mission [lbs]
+%    segment_fuel_fraction - Double. Fuel fraction calculated for mission 
+%    segment [unitless]
 % 
-% See also: togw_regression_loop()
+% See also: fuel_weight(), max_lift_to_drag(), main.m
 % Author:                          Juan
 % Version history revision notes:
 %                                  v1: 9/10/2024
