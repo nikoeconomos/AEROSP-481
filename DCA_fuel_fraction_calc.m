38631c3a28b1d021328eb758cd6e68a3488ec5d8
Niko Economos <economos@umich.edu>
2024-09-11 02:43:29 -0400

    updated weights file and overall file structure, changed variable names

--- DCA_fuel_fraction_calc.m
+++ DCA_fuel_fraction_calc.m
@@ -1,4 +1,4 @@
-function [mission_ff] = DCA_fuel_fraction(DCA_mission,lift_to_drag,cruise_fuel_fraction,loiter_fuel_fraction)
+function [mission_ff] = DCA_fuel_fraction_calc(DCA_mission,LD,cruise_fuel_fraction,loiter_fuel_fraction)
 % Description: This function calculates the total fuel fraction for the
 % Direct Counter-Air (DCA) Patroll Mission. It does this by using values
 % from a table of typical fuel fractions (table 2.2 in the metabook) for
@@ -15,7 +15,7 @@ function [mission_ff] = DCA_fuel_fraction(DCA_mission,lift_to_drag,cruise_fuel_f
 %    DCA_mission - Struct defined in generate_DCA_mission function. 
 %    Contains mission segment parameters and aircraft states 
 % 
-%    lift_to_drag - Double defined in max_lift_to_drag function. 
+%    LD - Double defined in max_lift_to_drag function. 
 %    Aircraft lift to drag ratio [unitless]
 %
 %    cruise_fuel_fraction - Function outputting double. 
@@ -28,22 +28,22 @@ function [mission_ff] = DCA_fuel_fraction(DCA_mission,lift_to_drag,cruise_fuel_f
 % --------------------------------------------
 %    mission_ff - Double. Fuel empty fraction calculated for DCA mission [unitless]
 % 
-% See also: generate_DCA_mission(), max_lift_to_drag(),
+% See also: generate_DCA_mission(), max_lift_to_drag(), TBU
 % Author:                          Juan
 % Version history revision notes:
 %                                  v1: 9/10/2024
 
-cruise_out_ff = cruise_fuel_fraction(DCA_mission.cruise_out.range,DCA_mission.cruise_out.tsfc,DCA_mission.cruise_out.flight_velocity,lift_to_drag);
+cruise_out_ff = cruise_fuel_fraction(DCA_mission.cruise_out.range,DCA_mission.cruise_out.tsfc,DCA_mission.cruise_out.flight_velocity,LD);
 
-loiter_ff = loiter_fuel_fraction(DCA_mission.loiter.endurance,DCA_mission.loiter.tsfc,lift_to_drag);
+loiter_ff = loiter_fuel_fraction(DCA_mission.loiter.endurance,DCA_mission.loiter.tsfc,LD);
 
-dash_ff = cruise_fuel_fraction(DCA_mission.dash.range,DCA_mission.dash.tsfc,DCA_mission.dash.flight_velocity,lift_to_drag);
+dash_ff = cruise_fuel_fraction(DCA_mission.dash.range,DCA_mission.dash.tsfc,DCA_mission.dash.flight_velocity,LD);
 
-combat_ff = cruise_fuel_fraction(DCA_mission.combat.range,DCA_mission.combat.tsfc,DCA_mission.combat.flight_velocity,lift_to_drag);
+combat_ff = cruise_fuel_fraction(DCA_mission.combat.range,DCA_mission.combat.tsfc,DCA_mission.combat.flight_velocity,LD);
 
-cruise_in_ff = cruise_fuel_fraction(DCA_mission.cruise_in.range,DCA_mission.cruise_in.tsfc,DCA_mission.cruise_in.flight_velocity,lift_to_drag);
+cruise_in_ff = cruise_fuel_fraction(DCA_mission.cruise_in.range,DCA_mission.cruise_in.tsfc,DCA_mission.cruise_in.flight_velocity,LD);
 
-reserve_ff = loiter_ff(DCA_mission.reserve.endurance,DCA_mission.reserve.tsfc,lift_to_drag);
+reserve_ff = loiter_ff(DCA_mission.reserve.endurance,DCA_mission.reserve.tsfc,LD);
 
 total_ff = DCA_mission.start_takeoff.ff*DCA_mission.climb.ff*cruise_out_ff*loiter_ff*dash_ff*combat_ff*cruise_in_ff*DCA_mission.descent.ff*reserve_ff;
 
--- loiter_fuel_fraction_calc.m
+++ loiter_fuel_fraction_calc.m
@@ -1,4 +1,4 @@
-function [segment_fuel_fraction] = loiter_fuel_fraction(segment_endurance,specific_fuel_capacity,lift_to_drag)
+function [segment_fuel_fraction] = loiter_fuel_fraction(segment_endurance,SFC,LD)
 % Description: This function calculates the fuel fraction for a given
 % loiter time when steady flight can be assumed. It uses equation 2.8 from the 
 % metabook meant to calculate loiter fuel fraction
@@ -8,10 +8,10 @@ function [segment_fuel_fraction] = loiter_fuel_fraction(segment_endurance,specif
 %    segment_endurance - Double defined in fuel_weight function. 
 %    Loitering time range [s] 
 % 
-%    specific_fuel_comsumption - Double defined in section 1 of main.m 
+%    SFC - Double defined in section 1 of main.m 
 %    Assumed specific fuel consumption for the PW F100-229 engine [kg/(kg*hr)]
 % 
-%    lift_to_drag -  Double defined in max_lift_to_drag function. 
+%    LD -  Double defined in max_lift_to_drag function. 
 %    Aircraft lift to drag ratio [unitless]
 % 
 % OUTPUTS:
@@ -24,6 +24,6 @@ function [segment_fuel_fraction] = loiter_fuel_fraction(segment_endurance,specif
 % Version history revision notes:
 %                                  v1: 9/10/2024
 
-segment_fuel_fraction = exp(-segment_endurance*specific_fuel_capacity/lift_to_drag);
+    segment_fuel_fraction = exp(-segment_endurance*SFC/LD);
 
 end
