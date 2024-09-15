84cb0ba92ccad9f285b84bffa83a444d1b9b0afe
unknown <johnchun@umich.edu>
2024-09-15 18:18:46 -0400

    Fixed prop cost calculations.

--- Cost/prop_cost_calc.m
+++ Cost/prop_cost_calc.m
@@ -19,19 +19,9 @@ function total_prop_cost = prop_cost_calc(aircraft)
 %                                  v1: 9/14/2024
 %                                  v1.1: 9/15/2024 - Modified to use
 %                                  aircraft struct input argument. (Joon)
+%                                  v1.2: 9/15/2024 - Separated engine
+%                                  maintenance cost into a different
+%                                  function.
     
-    
-    block_time = block_time_calc(aircraft);
-    % vvv PARAMETERIZE THIS SECTION IF NEEDED!!! vvv
-    base_year = 1993;
-    then_year = 2024;
-    bcef = 5.17053+0.104981*(base_year-2006);
-    tcef = 5.17053+0.104981*(then_year-2006);
-    cef = tcef/bcef;
-    % ^^^ PARAMETERIZE THIS SECTION IF NEEDED!!! ^^^
-    T_max_lbf = aircraft.propulsion.T_max*0.224809/aircraft.propulsion.engine_count; %Thrust of an individual engine, in lbf
-    Cml = (0.645+(0.05*T_max_lbf/10000))*(0.566+0.434/block_time)*aircraft.propulsion.maintenance_labor_rate;
-    Cmm = (25+(18*T_max_lbf/(10000*aircraft.propulsion.engine_count)))*(0.62+0.38/block_time)*cef;
-    engine_maint_cost = aircraft.propulsion.engine_count*(Cml+Cmm)*block_time;
-    total_prop_cost = aircraft.propulsion.engine_cost + aircraft.propulsion.fuel_cost + aircraft.propulsion.oil_cost + engine_maint_cost;
+    total_prop_cost = aircraft.propulsion.engine_cost + aircraft.propulsion.fuel_cost + aircraft.propulsion.oil_cost + aircraft.propulsion.engine_maintenance_cost;
 end
