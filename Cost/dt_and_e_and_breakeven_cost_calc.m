ec4e86469d018004e2bf0546e35497e3ffac117c
Juan Esteban Vega <juanvega@umich.edu>
2024-09-16 01:27:58 -0400

    created file to calculate research costs and estimate price tack on to break even with 60 sold units

---
+++ Cost/dt_and_e_and_breakeven_cost_calc.m
@@ -0,0 +1,8 @@
+function [program_dt_and_e_cost, breakeven_cost_addition] = dt_and_e_and_breakeven_cost_calc(inputArg1,inputArg2)
+%UNTITLED2 Summary of this function goes here
+%   Detailed explanation goes here
+
+
+outputArg1 = inputArg1;
+outputArg2 = inputArg2;
+end
