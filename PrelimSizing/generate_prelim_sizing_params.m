5e8bf791c5707ccd2d0e2fe061014d3b2cecd31d
Niko Economos <economos@umich.edu>
2024-09-17 18:59:45 -0400

    added prelim sizing folder

---
+++ PrelimSizing/generate_prelim_sizing_params.m
@@ -0,0 +1,50 @@
+% Aerosp 481 Group 3 - Libellula 
+function [aircraft] = generate_prelim_sizing_params(aircraft)
+% Description: This function generates a struct of aircraft parameters that
+% relate to assignment 4, preliminary sizing.
+% 
+% 
+% INPUTS:
+% --------------------------------------------
+%    aircraft - aircraft struct with specs
+% 
+% OUTPUTS:
+% --------------------------------------------
+%    aircraft - aircraft param with struct, updated with prelim sizing
+%    parameters
+%                       
+% 
+% See also: None
+% Author:                          Niko
+% Version history revision notes:
+%                                  v1: 9/14/2024
+
+%% ***CATEGORY*** %%
+%%%%%%%%%%%%%%%%%%%%
+
+aircraft.descriptiveName.placeholder = 0; %placeholder
+
+
+
+
+
+
+
+
+
+%% ***CATEGORY*** %%
+%%%%%%%%%%%%%%%%%%%%
+
+
+
+
+
+
+
+
+
+
+
+
+
+end
