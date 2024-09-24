0c250f2431cbd728b4035a2012b74bf338f17123
unknown <johnchun@umich.edu>
2024-09-23 22:28:16 -0400

    Better stall speed results

--- PrelimSizing/W_S_stall_speed_calc.m
+++ PrelimSizing/W_S_stall_speed_calc.m
@@ -18,12 +18,8 @@ function [W_S] = W_S_stall_speed_calc(aircraft, alt)
 %                                  v1: 9/14/2024
 
 
-[~, ~, Rho, a] = standard_atmosphere_calc(alt);
-
-cruise_speed = a*aircraft.performance.cruise_speed_mach;
-stall_speed = cruise_speed/1.25; %from metabook, cruise speed = 1.25 * stall
-
-
-W_S = 0.5*Rho*stall_speed^2*aircraft.aerodynamics.CL_cruise;
+[~, ~, Rho, ~] = standard_atmosphere_calc(alt);
+stall_speed = sqrt(2*aircraft.weight.togw*9.807/(Rho*aircraft.aerodynamics.CL_takeoff*aircraft.aerodynamics.Swet)); %Assume takeoff CL, at takeoff weight, at some higher altitude for buffer
+W_S = 0.5*Rho*stall_speed^2*aircraft.aerodynamics.CL_takeoff/9.807;
 
 end
