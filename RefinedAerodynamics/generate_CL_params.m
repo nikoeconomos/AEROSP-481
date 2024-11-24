775991b6d21c32a379c42356cba51e1be6964ad3
Niko Economos <economos@umich.edu>
2024-11-24 17:45:30 -0500

    new folder structure

---
+++ RefinedAerodynamics/generate_CL_params.m
@@ -0,0 +1,21 @@
+function [aircraft] = generate_CL_params(aircraft)
+% Description: 
+% 
+% INPUTS:
+% --------------------------------------------
+%    aircraft - aircraft struct
+%    
+% 
+% OUTPUTS:
+% --------------------------------------------
+%    plugs in Cl max values and gets CL for different conditions                   
+% 
+% See also:
+% Author:                          Juan
+% Version history revision notes:
+%                                  v1: 11/24/2024
+
+
+
+
+end
